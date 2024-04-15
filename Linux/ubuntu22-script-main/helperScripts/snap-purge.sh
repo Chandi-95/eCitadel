c4062924dd710b1690cce5e2f2bfe5778ca10117
bvillano05 <bvillano05@gmail.com>
2024-04-14 23:02:07 -0400

    Removed parts which remove important packages (core packages)

--- Linux/ubuntu22-script-main/helperScripts/snap-purge.sh
+++ Linux/ubuntu22-script-main/helperScripts/snap-purge.sh
@@ -6,17 +6,7 @@ remove_packages(){
     core_snaps=("snap-store" "gtk-common-themes" "gnome-system-monitor" "gnome-*" "core*" "snap-store" "snapd" "snapd-desktop-integration")
     #Find and remove all installed snaps (waits until later to remove core snaps bc they should be removed in certain order):
     installed_snaps=$(snap list | awk 'NR>1 {print $1}')
-    for i in installed_snaps; do
-        for j in core_snaps; do
-            if [$i = $j]; then
-                echo "$i\n$j"
-            fi
-        done
-    done
-    #Remove core snaps:
-    for snap in "${core_snaps[@]}"; do
-        snap remove "$snap"
-    done
+    
 }
 
 if command -v snap &> /dev/null
