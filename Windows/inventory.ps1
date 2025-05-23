ed5d98693cbf86c43571130c147fa6b553de39c0
chandi <chandikanhai@gmail.com>
2025-05-23 19:23:22 -0400

    implemented custom fields

--- Windows/inventory.ps1
+++ Windows/inventory.ps1
@@ -1,6 +1,6 @@
 $CssPath = (Join-Path (($MyInvocation.MyCommand.Path).substring(0,($MyInvocation.MyCommand.Path).indexOf("inventory.ps1")))  "report.css")
 
-$PreContent = "<div class='container'>`n<h1 style='text-align: center;'>Host Inventory Report</h1>`n"
+$PreContent = "<div class='container'>`n<h1 style='text-align: center;'>Host Information Report</h1>`n"
 
 $ComputerName = "<h2>Computer name: $env:computername</h2>"
 
@@ -9,7 +9,14 @@ if (Test-Path env:USERDNSDOMAIN) {
 	$DomainName = "<h2>Domain: $env:userdnsdomain</h2>"
 }
 
-$OSInfo = Get-CimInstance -Class Win32_OperatingSystem | ConvertTo-Html -Property Version,Caption,BuildNumber,OSArchitecture,ServicePackMajorVersion,WindowsDirectory -Fragment -PreContent "<h3>Operating System Information</h3>"
+$OSInfo = Get-CimInstance -Class Win32_OperatingSystem | 
+	Select-Object @{n='Name';e={$_.Caption}},
+		@{n='Version';e={$_.Version}},
+		@{n='Build';e={$_.BuildNumber}},
+		@{n='Architecture';e={$_.OSArchitecture}},
+		@{n='WinDir';e={$_.WindowsDirectory}},
+		@{n='SystemDir';e={$_.SystemDirectory}} |
+	ConvertTo-Html -Fragment -PreContent "<h3>Operating System Information</h3>"
 
 $LocalUserInfo = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount = 'True'" | ForEach-Object {
 	$userName = $_.Name
@@ -26,36 +33,38 @@ $LocalUserInfo = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount = 
     [PSCustomObject]@{
         Username         = $_.Name
         FullName         = $_.FullName
+		"Type"           = if ($_.SID -like "*-500") { "Admin" } elseif ($_.SID -like "*-501") { "Guest" } else { "User" }
+		Enabled		 	 = if ($_.Disabled -eq $false) { "Y" } else { "N" }
+		PwExpire	 	 = if ($_.PasswordExpires -eq $true) { "Y" } else { "N" }
+		PwReq 			 = if ($_.PasswordRequired -eq $true) { "Y" } else { "N" }
+		Groups           = $groups -join ", "
 		Description		 = $_.Description
-        Status           = if ($_.Disabled -eq $true) { "Disabled" } else { "Active" }
-        AccountType      = if ($_.SID -like "*-500") { "Administrator" } elseif ($_.SID -like "*-501") { "Guest" } else { "Standard" }
-		PasswordExpires	 = $_.PasswordExpires
-		PasswordRequired = $_.PasswordRequired
-        Groups           = $groups -join ", "
     }
 } | ConvertTo-Html -Fragment -PreContent "<h3>Local Users and Groups</h3>"
 
-# -Filter IPEnabled=TRUE
-$NetworkInfo = Get-CimInstance -Class Win32_NetworkAdapterConfiguration  | 
-	Select-Object Description,MACAddress,
-		@{Name='IpAddress';Expression={$_.IpAddress -join '; '}},
-		@{Name='IpSubnet';Expression={$_.IpSubnet -join '; '}}, 
-        @{Name='DefaultIPgateway';Expression={$_.DefaultIPgateway -join '; '}}, 
-        @{Name='DNSServerSearchOrder';Expression={$_.DNSServerSearchOrder -join '; '}} | 
+$NetworkInfo = Get-CimInstance -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE | 
+	Select-Object @{n='Name';e={$_.Description}},
+		@{n='MAC Address';e={$_.MACAddress}},
+		@{n='IP Address';e={$_.IpAddress -join '; '}},
+		@{n='Subnet';e={$_.IpSubnet -join '; '}}, 
+        @{n='Default Gateway';e={$_.DefaultIPgateway -join '; '}}, 
+        @{n='DNS Servers';e={$_.DNSServerSearchOrder -join '; '}} | 
 	ConvertTo-Html -Fragment -PreContent "<h3>Network Adapter Information</h3>"
 
 $TCPInfo = Get-NetTCPConnection -State Listen,Established -ErrorAction "SilentlyContinue" | 
 	Sort-Object state,localport | 
-	Select-Object LocalAddress,LocalPort,RemoteAddress,RemotePort,State,OwningProcess,
-		@{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}},
-		@{'Name'='CommandLine';'Expression'={(Get-CimInstance -Class Win32_Process -Filter "ProcessId = $($_.OwningProcess)").CommandLine}} | 
+	Select-Object @{n='Local IP:Port';e={$_.LocalAddress,$_.LocalPort -join ':'}},
+		@{n='Remote IP:Port';e={$_.RemoteAddress,$_.RemotePort -join ':'}},
+		@{n='State';e={if ($_.State -like "Listen") { "L" } elseif ($_.State -like "Established") { "E" } else { "" }}},
+		@{n='PID - Process';e={$_.OwningProcess,(Get-Process -Id $_.OwningProcess).ProcessName -join ' - '}},
+		@{n='Command Line';e={(Get-CimInstance -Class Win32_Process -Filter "ProcessId = $($_.OwningProcess)").CommandLine}} | 
 	ConvertTo-Html -Fragment -PreContent "<h3>Listening & Established TCP Connections</h3>"
 
 $UDPInfo = Get-NetUDPEndpoint | 
 	Sort-Object localport | 
-	Select-Object LocalAddress,LocalPort,OwningProcess,
-		@{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}},
-		@{'Name'='CommandLine';'Expression'={(Get-CimInstance -Class Win32_Process -Filter "ProcessId = $($_.OwningProcess)").CommandLine}} | 
+	Select-Object @{n='Local IP:Port';e={$_.LocalAddress,$_.LocalPort -join ':'}},
+		@{n='PID - Process';e={$_.OwningProcess,(Get-Process -Id $_.OwningProcess).ProcessName -join ' - '}},
+		@{n='Command Line';e={(Get-CimInstance -Class Win32_Process -Filter "ProcessId = $($_.OwningProcess)").CommandLine}} | 
 	ConvertTo-Html -Fragment -PreContent "<h3>UDP Sockets</h3>"
 
 # 32-bit on 64-bit
@@ -90,5 +99,12 @@ $SystemApps = $SystemApps.Replace("::","<br>")
 # if ((Get-CimInstance -Class Win32_OperatingSystem).Caption -match "Windows Server") {
 	# $FeatureInfo = Get-WindowsFeature | Where-Object {$_.InstallState -eq "Installed"} | ConvertTo-Html -Fragment -Property Name,Path -PreContent "<h3>Installed Roles and Features</h3>"
 # }
+
+# TODO: search for external package managers
+
 $Report = ConvertTo-Html -CssUri $CssPath -Body "$PreContent $ComputerName $DomainName $OSInfo $LocalUserInfo $NetworkInfo $TCPInfo $UDPInfo $SystemApps" -Title "Host Information Report" -PostContent "<p>Creation Date $(Get-Date)</p></div>" 
-$Report | Out-File .\inventory-report.html
+
+$timestamp = Get-Date -Format "yyyy-MM-dd-HHmmss"
+
+$Report | Out-File ".\inventory-$timestamp.html"
+# $Report | Out-File .\inventory.html
